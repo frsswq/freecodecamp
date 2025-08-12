57e3c6413b0a35da5b6bcbe7961e29ec4f6683c2
frsswq <farissaifuddin.work@gmail.com>
2025-08-12 20:05:45 +0700

    done project #3

--- project-urlshortener/index.js
+++ project-urlshortener/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 
@@ -7,7 +8,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +20,46 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const urlDatabase = {}
+
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body
+
+  if (!url) {
+    return res.json({error: 'invalid url'})
+  }
+
+  let checkUrl;
+  try {
+    checkUrl = new URL(url)
+  } catch (err) {
+    return res.json({error: 'invalid url'})
+  }
+
+  if (!['http:', 'https:'].includes(checkUrl.protocol)) {
+    return res.json({error: 'invalid url'})
+  }
+
+  const urlCode = Math.floor(10000 + Math.random() * 90000);
+  urlDatabase[urlCode] = url
+
+  res.json({
+    original_url: `${url}`,
+    short_url: urlCode
+  })
+})
+
+app.get('/api/shorturl/:url', (req, res) => {
+  const { url } = req.params
+  const getUrl = urlDatabase[url];
+
+  if (!getUrl) {
+   return res.json({error: 'invalid url'})
+  } 
+
+  res.redirect(getUrl)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
